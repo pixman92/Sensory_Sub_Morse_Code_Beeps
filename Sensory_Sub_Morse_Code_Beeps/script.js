f1b809b49a74b1164ea14cd2a1515d7b1c27de1c
ChristianB <gotrustd@verizon.net>
2019-07-14 20:57:57 -0700

    making all A-Z notes

--- Sensory_Sub_Morse_Code_Beeps/script.js
+++ Sensory_Sub_Morse_Code_Beeps/script.js
@@ -1,15 +1,70 @@
-// file to play sounds
 
-function loadSounds () {
-    createjs.Sound.registerSound("morseCode/short.ogg", "short");
-    createjs.Sound.registerSound("morseCode/long.ogg", "long");
+//=============================================
+// morse code break down
 
+var abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var morsecode =[
+    '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....',
+    '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.',
+    '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-',
+    '-.--', '--..'
+  ];
+//=============================================
+
+function makeWord(letter){
+    console.log('function that takes a letter and returns morse code', );
+    var pos = abc.indexOf(letter.toUpperCase());
+
+
+    return morsecode[pos];
 }
 
-function playShort () {
-    createjs.Sound.play("short");
+
+var code = "";
+async function makeBeepBasedOnLetter(letter){
+    console.log('changes box based on 1 letter of morse code');
+    code = makeWord(letter);
+    
+    for(var i in code){
+        if(code[i]=='.'){
+            playShort();
+        }
+        if(code[i]=='-'){
+            playLong();
+        }
+    
+    }
 }
 
-function playLong() {
-    createjs.Sound.play("long");
+
+//================================================
+//function for guessing words
+//NEXT? - work on 3 words and guessing and choosing correctly
+function getWord(){
+    return word = words[Math.floor(Math.random() * words.length)];
+}
+
+var wordsArray = []; var correctPos = -1;
+function createWords(){
+    wordsArray=[];
+    for(var i=0; i<2; i++){
+        wordsArray.push(getWord());
+    }
+
+    correctPos = Math.floor(Math.random()*wordsArray.length);
+    console.log('correctPos', correctPos);
+
+    return wordsArray;
+}
+//=============================================
+//function for taking in a string
+// this is the BIG kahuna,
+// main function for making flashing colors
+function splitString(stringMe){
+    var splited = stringMe.split('');
+
+    for(var i in splited){
+        makeBeepBasedOnLetter(splited[i]);
+    }
+    
 }
---
+++ Sensory_Sub_Morse_Code_Beeps/sounds.js
@@ -0,0 +1,142 @@
+// file to play sounds
+
+window.onload = ()=>{
+    // loadSounds();
+    document.getElementById('loadSounds').addEventListener('click', loadSounds);
+}
+
+
+function loadSounds () {
+    createjs.Sound.registerSound("morseCode/A_morse_code", "a");
+    createjs.Sound.registerSound("morseCode/B_morse_code", "b");
+    createjs.Sound.registerSound("morseCode/C_morse_code", "c");
+    createjs.Sound.registerSound("morseCode/D_morse_code", "d");
+    createjs.Sound.registerSound("morseCode/E_morse_code", "e");
+    createjs.Sound.registerSound("morseCode/F_morse_code", "f");
+    createjs.Sound.registerSound("morseCode/G_morse_code", "g");
+    createjs.Sound.registerSound("morseCode/H_morse_code", "h");
+    createjs.Sound.registerSound("morseCode/I_morse_code", "i");
+    createjs.Sound.registerSound("morseCode/J_morse_code", "j");
+    createjs.Sound.registerSound("morseCode/K_morse_code", "k");
+    createjs.Sound.registerSound("morseCode/L_morse_code", "l");
+    createjs.Sound.registerSound("morseCode/M_morse_code", "m");
+    createjs.Sound.registerSound("morseCode/N_morse_code", "n");
+    createjs.Sound.registerSound("morseCode/O_morse_code", "o");
+    createjs.Sound.registerSound("morseCode/P_morse_code", "p");
+    createjs.Sound.registerSound("morseCode/Q_morse_code", "q");
+    createjs.Sound.registerSound("morseCode/R_morse_code", "r");
+    createjs.Sound.registerSound("morseCode/S_morse_code", "s");
+    createjs.Sound.registerSound("morseCode/T_morse_code", "t");
+    createjs.Sound.registerSound("morseCode/U_morse_code", "u");
+    createjs.Sound.registerSound("morseCode/V_morse_code", "v");
+    createjs.Sound.registerSound("morseCode/W_morse_code", "w");
+    createjs.Sound.registerSound("morseCode/X_morse_code", "x");
+    createjs.Sound.registerSound("morseCode/Y_morse_code", "y");
+    createjs.Sound.registerSound("morseCode/Z_morse_code", "z");
+    
+
+}
+
+function playA () {
+    createjs.Sound.play("a");
+}
+
+function playB() {
+    createjs.Sound.play("b");
+}
+
+function playC() {
+    createjs.Sound.play("b");
+}
+
+function playD() {
+    createjs.Sound.play("b");
+}
+
+function playE() {
+    createjs.Sound.play("b");
+}
+
+function playF() {
+    createjs.Sound.play("b");
+}
+
+function playG() {
+    createjs.Sound.play("b");
+}
+
+function playH() {
+    createjs.Sound.play("b");
+}
+
+function playI() {
+    createjs.Sound.play("b");
+}
+
+function playJ() {
+    createjs.Sound.play("b");
+}
+
+function playK() {
+    createjs.Sound.play("b");
+}
+
+function playL() {
+    createjs.Sound.play("b");
+}
+
+function playM() {
+    createjs.Sound.play("b");
+}
+
+function playN() {
+    createjs.Sound.play("b");
+}
+
+function playO() {
+    createjs.Sound.play("b");
+}
+
+function playP() {
+    createjs.Sound.play("b");
+}
+
+function playQ() {
+    createjs.Sound.play("b");
+}
+
+function playR() {
+    createjs.Sound.play("b");
+}
+
+function playS() {
+    createjs.Sound.play("b");
+}
+
+function playT() {
+    createjs.Sound.play("b");
+}
+
+function playU() {
+    createjs.Sound.play("b");
+}
+
+function playV() {
+    createjs.Sound.play("b");
+}
+
+function playW() {
+    createjs.Sound.play("b");
+}
+
+function playX() {
+    createjs.Sound.play("b");
+}
+
+function playY() {
+    createjs.Sound.play("b");
+}
+
+function playZ() {
+    createjs.Sound.play("b");
+}
